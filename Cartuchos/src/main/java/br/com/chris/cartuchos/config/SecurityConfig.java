ee69cc4a3e547668b7e8a32d5003f0501327082c
Christopher <chrissantista@gmail.com>
2017-04-18 21:47:50 -0300

    aplicada autenticação.

--- Cartuchos/src/main/java/br/com/chris/cartuchos/config/SecurityConfig.java
+++ Cartuchos/src/main/java/br/com/chris/cartuchos/config/SecurityConfig.java
@@ -1,26 +1,49 @@
 package br.com.chris.cartuchos.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder builder)
 			throws Exception{
 		builder
-			.inMemoryAuthentication()
-			.withUser("chris").password("123").roles("USER")
-			.and()
-			.withUser("admin").password("321").roles("ADMIN");
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from usuario where username = ?")
+				.authoritiesByUsernameQuery("select username, role from usuario where username = ?");
+			/*.inMemoryAuthentication()
+				.withUser("chris").password("123").roles("USER")
+				.and()
+				.withUser("admin").password("321").roles("ADMIN");*/
 	}
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
+			.formLogin().and()/*.loginPage("/login").permitAll()
+			.failureUrl("/login?erro=true")
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/login")
+			.and()*/
 			.authorizeRequests()
-			.antMatchers("/**").hasAuthority("USER")
-			.anyRequest().authenticated();
+				.antMatchers("/**").hasAnyAuthority("USER")
+				.antMatchers("/admin/*").hasAuthority("ADMIN")
+				.anyRequest().authenticated();
 	}
+	
 }
--- Cartuchos/src/main/resources/import.sql
+++ Cartuchos/src/main/resources/import.sql
@@ -1,1 +1,3 @@
 insert into departamento (id, descricao, ativo) values (null,'Cartuchos', true);
+
+insert into usuario (username,password,enabled,role) values ('chris','123456',true,'USER'),	('admin','111111',true,'ADMIN');
